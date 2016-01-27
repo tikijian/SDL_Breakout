877cd29a6acd1ccba34fe36c6f2d78d8c47b084d
tikijian <timurt1988@gmail.com>
2016-01-27 22:22:21 +0300

    Initial Commit

---
+++ SDL_Breakout/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SDL_Breakout
+//
+//  Created by Тимур on 27.01.16.
+//  Copyright © 2016 Tikijian. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
